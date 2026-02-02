31c08360d09006f862e7f0780a72c20d195ecf37
mohitambani00-a11y <mohitambani00@gmail.com>
2026-02-02 21:37:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,14 +2,16 @@ const noBtn = document.getElementById("no");
 const yesBtn = document.getElementById("yes");
 const msg = document.getElementById("msg");
 
+// Move NO button randomly
 noBtn.addEventListener("mouseover", () => {
-  const x = Math.random() * window.innerWidth;
-  const y = Math.random() * window.innerHeight;
+  const x = Math.random() * (window.innerWidth - 100);
+  const y = Math.random() * (window.innerHeight - 50);
 
   noBtn.style.left = x + "px";
   noBtn.style.top = y + "px";
 });
 
+// Yes message
 yesBtn.addEventListener("click", () => {
   msg.innerText = "Yayyy! Best Valentine friend ever 💖✨";
 });
